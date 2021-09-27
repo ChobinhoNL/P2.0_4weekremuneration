b538129b0a6a9fd8a8c9cc812108b33bc2c3347c
Michail Levitin <djo.chobo@gmail.com>
2021-09-27 12:45:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from datetime import *
 import datetime
+from flask import Flask, request
+
+app = Flask(__name__)
 
 def calc_date(start):
     today_date = date.today()
@@ -8,13 +11,20 @@ def calc_date(start):
         start = start + four_weeks
     if today_date < start:
         start = start - datetime.timedelta(days = 1)
-    last_day = f"{start.day}-{start.month}-{start.year}""
+    last_day = f"{start.day}-{start.month}-{start.year}"
     return last_day 
 
+@app.route('/', methods=["GET"])
+def start():
+    start = request.args['date']
+    # d1, m1, y1 = [int(x) for x in start("Wat is de startdatum van het contract? (DD-MM-YYYY please): ").split('-')]
+    d1 = int(f"{start[0]}{start[1]}")
+    m1 = int(f"{start[3]}{start[4]}")
+    y1 = int(f"{start[6:]}")
+    start_date = date(y1, m1, d1)
 
-d1, m1, y1 = [int(x) for x in input("Wat is de startdatum van het contract? (DD-MM-YYYY please): ").split('-')]
-start_date = date(y1, m1, d1)
+    result = calc_date(start_date)
 
-print(calc_date(start_date))
+    return str(result) #calc_date(start_date)
 
-input("Druk op <Enter> om af te sluiten.") 
+app.run()
